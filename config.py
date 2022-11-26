a670755e3dfd87913032c68ea7368d9748941344
SaintPS <116155714+SaintPS@users.noreply.github.com>
2022-11-26 14:24:44 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+TOKEN = "5880702605:AAEmBFrYQqUzqfJTvQVd02sQIYH-A7Hb888"
+
+keys = {
+    "euro": "EUR",
+    "usd": "USD",
+    "rub": "RUB",
+    "bitcoin": "BTC",
+    "ethereum": "ETH",
+    "bnb": "BNB",
+    "riple": "XRP",
+    "dogecoin": "DOGE",
+    "cardano": "ADA",
+    "polygon": "MATIC",
+    "polkadot": "DOT",
+    "litecoin": "LTC",
+    "solana": "SOL",
+    "tether": "USDT",
+    }
